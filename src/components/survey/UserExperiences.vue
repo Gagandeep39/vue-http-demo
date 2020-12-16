9f3b4648e11ebaa8d105216f3ba5e1f47141e5b0
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-12-16 06:14:23 +0000

    Implemented GET logic

--- src/components/survey/UserExperiences.vue
+++ src/components/survey/UserExperiences.vue
@@ -3,13 +3,15 @@
     <base-card>
       <h2>Submitted Experiences</h2>
       <div>
-        <base-button>Load Submitted Experiences</base-button>
+        <base-button @click="loadExperience"
+          >Load Submitted Experiences</base-button
+        >
       </div>
       <ul>
         <survey-result
           v-for="result in results"
           :key="result.id"
-          :name="result.name"
+          :name="result.userName"
           :rating="result.rating"
         ></survey-result>
       </ul>
@@ -18,12 +20,37 @@
 </template>
 
 <script>
+import axios from 'axios';
 import SurveyResult from './SurveyResult.vue';
 
 export default {
-  props: ['results'],
+  // props: ['results'],
   components: {
     SurveyResult,
+  },
+  data() {
+    return {
+      results: [],
+    };
+  },
+  methods: {
+    loadExperience() {
+      axios
+        .get(`${process.env.VUE_APP_URL}`)
+        .then((res) => {
+          const body = res.data;
+          const results = [];
+          for (const data in body) {
+            results.push({
+              id: data,
+              userName: body[data].userName,
+              rating: body[data].rating,
+            });
+          }
+          this.results = results;
+        })
+        .catch((err) => console.log(err));
+    },
   },
 };
 </script>
